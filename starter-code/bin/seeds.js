191d8e9db2652353f3c9e2723e6985a5f680f9c1
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-09-04 12:12:34 -0400

    done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,32 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+
+const DroneModel = require('../models/drone.js');
+
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const droneData = [
+  { droneName: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { droneName: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { droneName: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+DroneModel.create(
+  //1st argument -> array of products to save
+  droneData,
+
+  //2nd argument -> callback
+  (err, dronesAfterSave) => {
+    //check for errors
+    if(err){
+      console.log('Create error ^^;');
+      console.log(err);
+      return;
+    }
+    //show success feedback
+    dronesAfterSave.forEach((oneDrone) => {
+        console.log('Drone---> ' + oneDrone.droneName );
+    });
+  }
+); //close DroneModel.create(...
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -2,20 +2,63 @@ const express = require('express');
 
 // require the Drone model here
 
+const DroneModel = require('../models/drone.js');
+
 const router = express.Router();
 
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2
-});
+    DroneModel.find((err, allDrones) => {
+      //If there's a database error... return to avoid showing the view
+      if (err) {
+          //Skip to the error handler middleware
+          next(err);
+          //if error ... shit
+          return;
+      }
+      //do a find in the database
+      //send the RESULTS to the view
+      res.locals.drones = allDrones;
+
+      res.render('drones/index.ejs');
+    });//close ProductModel.find(..
+});//close GET /products
+
+
+
+
+
 
 
 router.get('/drones/new', (req, res, next) => {
   // Iteration #3
+  res.render('drones/new.ejs');
 });
 
 router.post('/drones', (req, res, next) => {
   // Iteration #3
+  const theDrone = new DroneModel({
+    droneName: req.body.droneName,
+    propellers: req.body.dronePropellor,
+    maxSpeed: req.body.droneMaxSpeed
+  });
+
+  theDrone.save((err) => {
+        if (err) {
+            //Skip to the error handler middleware
+            next(err);
+            //if error ... shit
+            return;
+        }
+
+        // res.render('product-views/product-form.ejs'); <--DON'T EVER DO THIS
+        //if success.... yay! but also
+        //     STEP #3 redirect  //
+        // ALWAYS redirect after a successful to POST to avoid resubmitting/dublicates
+        res.redirect('/drones');
+        // you can only redirect to a URL
+    });
+
 });
 
 module.exports = router;
